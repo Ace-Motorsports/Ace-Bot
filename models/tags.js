1363f3d5c8b72ee078ff3511d2506cdd905eef63
mbacon89 <mbacon89@gmail.com>
2025-10-31 11:58:18 +0000

    fix issue with keys

--- models/tags.js
+++ models/tags.js
@@ -1,22 +1,27 @@
 module.exports = (sequelize, DataTypes) => {
-    return sequelize.define('tags', {
-        discord_id: {
-            type: DataTypes.STRING,
-            unique: 'compositeIndex'
-        },
-        guild_id: {
-            type: DataTypes.STRING,
-            unique: 'compositeIndex'
-        },
-        iRacing_ID: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        display_license: {
-            type: DataTypes.BOOLEAN,
-            defaultValue: true,
-        },
-    }, {
-        timestamps: true,
-    });
+	return sequelize.define('tags', {
+		id: {
+			type: DataTypes.INTEGER,
+			primaryKey: true,
+			autoIncrement: true,
+		},
+		discord_id: {
+			type: DataTypes.STRING,
+			unique: 'compositeIndex',
+		},
+		guild_id: {
+			type: DataTypes.STRING,
+			unique: 'compositeIndex',
+		},
+		iRacing_ID: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		display_license: {
+			type: DataTypes.BOOLEAN,
+			defaultValue: true,
+		},
+	}, {
+		timestamps: true,
+	});
 };
