0033339b654ec0b9be53c08d35885072963ed1ff
mbacon89 <mbacon89@gmail.com>
2025-10-31 11:06:39 +0000

    see who is linked on the server

---
+++ commands/utility/wholinked.js
@@ -0,0 +1,35 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { Tags } = require('../../database');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('wholinked')
+		.setDescription('Lists all users in the server with a linked iRacing ID.'),
+	async execute(interaction) {
+		const guildId = interaction.guild.id;
+
+		try {
+			const tagList = await Tags.findAll({ where: { guild_id: guildId } });
+
+			if (tagList.length === 0) {
+				return interaction.reply('No users in this server have linked their iRacing ID.');
+			}
+
+            const promises = tagList.map(async (tag) => {
+                const member = await interaction.guild.members.fetch(tag.discord_id).catch(() => null);
+                if (member) {
+                    return `${member.user.tag} (iRacing ID: ${tag.iRacing_ID})`;
+                } else {
+                    return `User with ID ${tag.discord_id} (iRacing ID: ${tag.iRacing_ID}) is no longer in the server.`;
+                }
+            });
+
+            const memberList = await Promise.all(promises);
+			await interaction.reply(`**Users with linked iRacing IDs:**\n${memberList.join('\n')}`);
+		}
+		catch (error) {
+			console.error('Error listing linked users:', error);
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+	},
+};
